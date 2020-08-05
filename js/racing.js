95f6b986675f4e852f2a905bcc6ba957f7cc9eaf
wujunweis <wujunweis@foxmail.com>
2020-08-06 00:18:31 +0800

    update racing and repair all

--- js/racing.js
+++ js/racing.js
@@ -11,19 +11,59 @@
 
     function hideItem() {
         const itemTip = $("#racingGame").find("i");
-
         if (racingGameStatus === 1 && !itemTip.is(":hidden")) {
             itemTip.hide();
         }
     }
 
     $("#racingFooterUl li").click(item => {
-        const index = item.target.getAttribute('tag');
-        if (index) {
-
+        const index = item.target.getAttribute("tag");
+        if (index && index * 1 < 4) {
+            const rClassName = $("#racingFooterUl")[0].getAttribute("class");
+            if ($("#racingFooterUl").hasClass(bgList[index - 1])) {
+                return;
+            } else {
+                $("#racingFooterUl").removeClass(rClassName);
+                $("#racingFooterUl").toggleClass(bgList[index - 1], true);
+            }
+            switch (index * 1) {
+                case 1:
+                    $("#racingContentColor").show();
+                    $("#racingContentCar").hide();
+                    $("#racingContentEnv").hide();
+                    break;
+                case 2:
+                    $("#racingContentCar").show();
+                    $("#racingContentColor").hide();
+                    $("#racingContentEnv").hide();
+                    break;
+                case 3:
+                    $("#racingContentColor").hide();
+                    $("#racingContentCar").hide();
+                    $("#racingContentEnv").show();
+                    break;
+                default:
+                    break;
+            }
         }
+        if (index === "4") {
+            console.log("save");
+        }
+        racingUlIndex = index;
     });
-    $("#racingFooterUl").addClass('bg-color')
+    $("#racingContent p img").click(item => {
+        if (item.target.src.includes("click")) {
+            return;
+        } else {
+            for (let element of $("#racingContent img")) {
+                element.src = element.src.replace("-click", "");
+            }
+            // set current img check status
+            item.target.src = item.target.src.replace(".png", "-click.png");
+        }
+
+    });
+    $("#racingFooterUl").addClass("bg-color");
     root.racingObj = {
         startRacing: startRacing,
         hideItem: hideItem,
